7b585533ee438f062db7a8fcb3439b81acb7fd8f
Dmytro <dmytrosablin@gmail.com>
2023-12-24 22:34:17 +0200

    second commit

--- mole.js
+++ mole.js
@@ -8,6 +8,7 @@ window.onload = function() {
 }
 
 function setGame() {
+
     //set up the grid in html
     for (let i = 0; i < 9; i++) { //i goes from 0 to 8, stops at 9
         //<div id="0-8"></div>
@@ -16,8 +17,8 @@ function setGame() {
         tile.addEventListener("click", selectTile);
         document.getElementById("board").appendChild(tile);
     }
-    setInterval(setMole, 1000); // 1000 miliseconds = 1 second, every 1 second call setMole
-    setInterval(setPlant, 2000); // 2000 miliseconds = 2 seconds, every 2 second call setPlant
+    setInterval(setMole, 700); // 1000 miliseconds = 1 second, every 1 second call setMole
+    setInterval(setPlant, 1500); // 2000 miliseconds = 2 seconds, every 2 second call setPlant
 }
 
 function getRandomTile() {
